b250bf062cfa94056d3dec8065ff6eae98ee50cd
regrets123 <lotus.saxe@gmail.com>
2018-12-05 16:10:37 +0100

    Adding scripts
    Pickupables and gamemanager.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cuppsats
+{
+    public class GameManager : MonoBehaviour
+    {
+        public GameObject gameManager;
+        public int Score;
+
+        // Use this for initialization
+        void Start()
+        {
+            gameManager = this.gameObject;
+        }
+        
+        
+    }
+}
---
+++ Assets/Scripts/PickupAble.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Collections;
+
+namespace Cuppsats
+{
+    public class PickupAble : MonoBehaviour
+    {
+        [SerializeField]
+        private int pickUpValue;
+
+
+        void Start ()
+        {
+            pickUpValue = 5;
+
+        }
+        
+        public void PickMeUp(GameManager player)
+        {
+            player.Score += pickUpValue;
+            Destroy(gameObject);
+        }
+
+        private void OnTriggerEnter (Collider player)
+        {
+
+            if (Input.GetKeyDown(KeyCode.E))
+                PickMeUp(player.GetComponentInParent<GameManager>());
+            
+        }
+        
+    }
+}
